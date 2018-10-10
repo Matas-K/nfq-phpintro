822aee8dab983aeacd601c7bb89eb9b5b267c393
Matas Kulingauskas <matask@gmail.com>
2018-10-10 21:58:08 +0300

    Initiated files and basic functions

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'src/functions.php';
+require 'src/functions_strict.php';
+
+header('Content-Type: text/plain; charset=utf-8');
+
+echo 'calculateHomeWorkSum: ';
+echo \calculateHomeWorkSum(3, 2.2, '1');
+echo PHP_EOL;
+
+echo '\Nfq\Akademija\Not_Typed\calculateHomeWorkSum: ';
+echo \Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1');
+echo PHP_EOL;
+
+echo '\Nfq\Akademija\Soft\calculateHomeWorkSum: ';
+echo \Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1');
+echo PHP_EOL;
+
+echo '\Nfq\Akademija\Strict\calculateHomeWorkSum: ';
+echo \Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1');
+echo PHP_EOL;
---
+++ src/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace {
+	function calculateHomeWorkSum(...$numbers){
+		return array_sum($numbers);
+	}
+}
+
+namespace Nfq\Akademija\Not_Typed {
+	function calculateHomeWorkSum(...$numbers): int{
+		return array_sum($numbers);
+	}
+}
+
+namespace Nfq\Akademija\Soft {
+	function calculateHomeWorkSum(int ...$numbers): int{
+		return array_sum($numbers);
+	}
+}
